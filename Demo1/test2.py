fde8959c86cbae2c941663733c2132af512f1f14
MinTimmy <timmygood6590@gmail.com>
2020-10-29 22:02:51 +0800

    nre Counting_sort

--- Demo1/test2.py
+++ Demo1/test2.py
@@ -1,115 +1,31 @@
 import sys
-import random
-import time
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-
-class Main(QMainWindow):
-
-    def __init__(self, parent = None):
-        QMainWindow.__init__(self, parent)
-        self.InitUi()
-
-    def InitUi(self):
-        self.textBox = self.CreateTextbox()
-        self.button = self.CreateButton()
-
-        self.show()
-        self.setStyleSheet('QFrame{background-color:rgb(0,0,255)}')
-        self.numbers = [] # 使用者輸入的數字
-        self.output_answer = [[0 for i in range(10)] for j in range(100)] # 紀錄要輸出的數字
-        self.count = 0 # 紀錄要交換幾次
-        
-    def CreateTextbox(self):
-        #lb = QLabel(self, text = "TextBox: ")
-        #lb.move(180,60)
-        textBox = QLineEdit(self)
-        textBox.resize(500,30)
-        textBox.move(250,60)
-        return textBox
-
-    def CreateButton(self):
-        button = QPushButton(self, text = "OK")
-        button.resize(30,30)
-        button.move(800, 60)
-        button.clicked.connect(lambda: self.setAmount())
-        button.clicked.connect(lambda: print("OK"))
-        return button
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsTextItem, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QWheelEvent
 
 
-    def setAmount(self):
-        text = self.textBox.text()
-        #text = "5,2,7,1,7,3"
-        self.check = True
+class MyView(QGraphicsView):
+    def wheelEvent(self, event: QWheelEvent):
+        if event.modifiers() == Qt.ShiftModifier:
+            self.horizontalScrollBar().wheelEvent(event)
+        else:
+            self.verticalScrollBar().wheelEvent(event)
 
-        #處理字串 從 "5,2,7,1,7,3" 變成 self.numbers = [5,2,7,1,7,3]
-        i = 0
-        tmp = ""
-        c = []
-        while i < len(text):
-            if text[i] == ",":
-                c.append(int(tmp))
-                tmp = ""
-            if text[i] != ",":
-                tmp = tmp + text[i]
-            i += 1
-            print(tmp)
-        c.append(int(tmp))
-        #print(c)
-        self.numbers = c
-        self.bubble_sort()
-        
-
-    def bubble_sort(self):
-        for i in range(len(self.numbers)):
-            self.output_answer[self.count][i] = self.numbers[i]
-        swapped = True
-        while swapped:
-            swapped = False
-            for i in range(len(self.numbers) - 1):
-                if self.numbers[i] < self.numbers[i + 1]: 
-                    self.numbers[i], self.numbers[i + 1] = self.numbers[i + 1], self.numbers[i]
-                    self.count += 1
-                    for i in range(len(self.numbers)):
-                        self.output_answer[self.count][i] = self.numbers[i]
-                    swapped = True
-        self.count += 1
-        for i in range(len(self.numbers)):
-            self.output_answer[self.count][i] = self.numbers[i]    
-        self.create_label()
-        
-    def create_label(self):
-        x = 200
-        y = 200
-        for i in range(self.count):
-            for j in range(len(self.numbers)):
-                temp = self.output_answer[i][j] * 20
-                lb = QLabel(self, text = str(self.output_answer[i][j]))
-                lb.setAlignment(Qt.AlignCenter)
-                lb.resize(temp, temp) 
-                lb.move(x-temp/2,y-temp/2)
-                lb.setStyleSheet("border: 3px solid blue; border-radius:"+str(temp/2)+"px;") 
-                lb.show()
-                self.button.clicked.connect(lambda: lb.hide()) #clear the label
-                x += 200
-            y += 200
-            x = 200
-        self.numbers = []
-        self.output_answer = [[0 for i in range(10)] for j in range(100)]
-        self.count = 0
-        
-    def label(self, t, x, y):
-        pass
-        
-    
-def main():
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = Main()
-    main_window.resize(10000,10000)
-    main_window.show()
-    app.exec_()
+    scene = QGraphicsScene()
+    view = MyView(scene)
+    
+    # Generate 200 objects, 100 on x and 100 on y.
+    for i in range(100):
+        obj_on_x = QGraphicsTextItem("X" + str(i))
+        obj_on_x.setX(i * 100)
+        scene.addItem(obj_on_x)
 
-if __name__ == "__main__":
-    main()
+        obj_on_y = QGraphicsTextItem("Y" + str(i))
+        obj_on_y.setY(i * 100)
+        scene.addItem(obj_on_y)
+
+    view.show()
+    sys.exit(app.exec_())
 
